505b4c05d60968a59cc343c3aaf0b0d03e5d8a30
Craig Millard <craig@Craigs-iMac.local>
2021-07-10 21:51:31 -0800

    created hexagon shape

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,36 @@
 /// <reference types="../node_modules/types-for-adobe/AfterEffects/2018"/>
 
 // Create a layer
-var midiLayer = app.project.item(4).name;
-alert(midiLayer);
+var _midiLayer = app.project.item(4).layer(1);
+var _rectangle1 = midiLayer.property("Contents").property("Rectangle 1");
+
+$.writeln(midiLayer.property("Contents").property("Rectangle 1").matchName);
+
+// Find comp
+// Add shape layer
+// Add "ADBE Vector Group"
+app.beginUndoGroup("Process");
+var comp = app.project.items.addComp("Test Comp", 1920, 1080, 1, 10, 30)
+comp.openInViewer();
+var shapeLayer = comp.layers.addShape();
+shapeLayer.name = "Rectangle"
+var rectangleGroup = shapeLayer.property("Contents").addProperty("ADBE Vector Group")
+rectangleGroup.name = "Rectangle"
+
+// Path group is different
+var pathGroup = shapeLayer.property("Contents").property("Rectangle").property("Contents").addProperty("ADBE Vector Shape - Star");
+pathGroup.property("Type").setValue(2);
+pathGroup.property("Points").setValue(6);
+
+// Fill is same
+var strokeGroup = shapeLayer.property("Contents").property("Rectangle").property("Contents").addProperty("ADBE Vector Graphic - Stroke");
+
+// Fill is same
+var fillGroup = shapeLayer.property("Contents").property("Rectangle").property("Contents").addProperty("ADBE Vector Graphic - Fill");
+
+app.endUndoGroup();
+
+var myShape = new Shape();
+myShape.vertices = [[0,0], [0,100], [100,100], [100,0]];
+myShape.closed = true;
+
