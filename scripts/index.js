dd1386af89317bc4b4b6ccd18b35d493cffc83a0
Craig Millard <craig@Craigs-iMac.local>
2021-07-16 22:35:20 -0800

    use json object for notes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,262 @@
-/// <reference types="../node_modules/types-for-adobe/AfterEffects/2018"/>
+var scale = {
+    "header": {
+      "keySignatures": [],
+      "meta": [],
+      "name": "\u0000",
+      "ppq": 96,
+      "tempos": [],
+      "timeSignatures": [
+        {
+          "ticks": 0,
+          "timeSignature": [
+            4,
+            4
+          ],
+          "measures": 0
+        },
+        {
+          "ticks": 0,
+          "timeSignature": [
+            4,
+            4
+          ],
+          "measures": 0
+        }
+      ]
+    },
+    "tracks": [
+      {
+        "channel": 0,
+        "controlChanges": {},
+        "pitchBends": [],
+        "instrument": {
+          "family": "piano",
+          "name": "acoustic grand piano",
+          "number": 0
+        },
+        "name": "\u0000",
+        "notes": [
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 48,
+            "time": 0.25,
+            "velocity": 0.8818897637795275
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 144,
+            "time": 0.75,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 240,
+            "time": 1.25,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 336,
+            "time": 1.75,
+            "velocity": 1
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 432,
+            "time": 2.25,
+            "velocity": 0.8818897637795275
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 528,
+            "time": 2.75,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 624,
+            "time": 3.25,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 720,
+            "time": 3.75,
+            "velocity": 1
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 816,
+            "time": 4.25,
+            "velocity": 0.8818897637795275
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 912,
+            "time": 4.75,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1008,
+            "time": 5.25,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1104,
+            "time": 5.75,
+            "velocity": 1
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1200,
+            "time": 6.25,
+            "velocity": 0.8818897637795275
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1296,
+            "time": 6.75,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1392,
+            "time": 7.25,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1488,
+            "time": 7.75,
+            "velocity": 1
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1584,
+            "time": 8.25,
+            "velocity": 0.8818897637795275
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1680,
+            "time": 8.75,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1776,
+            "time": 9.25,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1872,
+            "time": 9.75,
+            "velocity": 1
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 1968,
+            "time": 10.25,
+            "velocity": 0.8818897637795275
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 2064,
+            "time": 10.75,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 2160,
+            "time": 11.25,
+            "velocity": 0.7874015748031497
+          },
+          {
+            "duration": 0.125,
+            "durationTicks": 24,
+            "midi": 44,
+            "name": "G#2",
+            "ticks": 2256,
+            "time": 11.75,
+            "velocity": 1
+          }
+        ],
+        "endOfTrackTicks": 2280
+      }
+    ]
+  };
 
 function getItemByName(name){
     for (i = 1; i <= app.project.numItems; i++) {
@@ -38,17 +296,39 @@ function drawRectangleInLayer(layer, rectName, position, size){
 var _sound_Template = getItemByName("Template");
 var _midiLayer = getLayerByName("Top1", _sound_Template);
 
+var xOffset = 45
+var yOffset = -375
+
+
+// write first block
+for (n = 0; n < scale["tracks"][0].notes.length; n++) {
+    var note = scale["tracks"][0].notes[n];
+    $.writeln(note.midi);
+    $.writeln(note.ticks);
+    $.writeln(note.durationTicks);
+    $.writeln(note.duration);
+    $.writeln(note.velocity);
+    $.writeln(note.time);
+
+
+// Rectangle bounds
+if (note.time < 8){
+  drawRectangleInLayer(_midiLayer, "Note " + n, [xOffset,yOffset+(note.time * -16)], [10, note.duration * -3]);
+}
+
+
+}
+
 // // Test drawing keys 
 // for (i = 1; i <= 25; i++) {
 //     drawRectangleInLayer(_midiLayer, "Rectangle " + i, [0,i*24], [180,10]);
 //   } 
 
-var xOffset = 45
-var yOffset = -430
+// subdivide 480 into the 25 keys ()
 
-// Rectangle bounds
-drawRectangleInLayer(_midiLayer, "Rectangle " + i, [xOffset,yOffset], [480,120]);
+// subdivide time 120 / 8 = 15 pixels per second 
 
+// melody B3 to C5
 
 
 
